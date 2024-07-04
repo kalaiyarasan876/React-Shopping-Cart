8e295066be1e7858d83c97e0c1d1dc9863ae4f73
kalaiyarasan876 <160699419+kalaiyarasan876@users.noreply.github.com>
2024-07-04 16:00:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Cart from './components/Cart'
+import { items } from './components/Data'
+import Navbar from './components/Navbar'
+import Product from './components/Product'
+import ProductDetail from './components/ProductDetail'
+import SearchItem from './components/SearchItem'
+
+
+const App = () => {
+  const [data, setData] = useState([...items])
+  const [cart, setCart] = useState([])
+  return (
+    <>
+    <Router>
+    <Navbar cart={cart} setData={setData} />
+    <Routes>
+      <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
+      <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
+      <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
+      <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+    </Routes>
+  
+    </Router>
+    </>
+  )
+}
+
+export default App
